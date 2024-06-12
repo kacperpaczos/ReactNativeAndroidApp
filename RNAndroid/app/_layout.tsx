1ded2b05b0f381f6dbc422b5bb98b3f1e8cd06c3
Kacper Paczos <kacper.paczos@linux.pl>
2024-06-12 23:42:29 +0200

    I initialized watermelondb, added Spinner while loading.

--- RNAndroid/app/_layout.tsx
+++ RNAndroid/app/_layout.tsx
@@ -3,11 +3,15 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
 import 'react-native-reanimated';
+import { UserDAO } from '@/database/WatermelonDB/dao/UserDAO';
+import { Provider as PaperProvider, ActivityIndicator } from 'react-native-paper'; // Import Provider i ActivityIndicator z react-native-paper
 
 import { useColorScheme } from '@/components/useColorScheme';
 
+// Przenieś wszystkie eksporty na górę modułu
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
@@ -21,11 +25,20 @@ export const unstable_settings = {
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
   });
+  const [isDatabaseEmpty, setDatabaseEmpty] = useState(true); // Dodaj stan dla pustej bazy danych
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
@@ -35,14 +48,28 @@ export default function RootLayout() {
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
+      checkDatabase(); // Sprawdź bazę danych po załadowaniu czcionek
     }
   }, [loaded]);
 
-  if (!loaded) {
-    return null;
+  const checkDatabase = async () => {
+    const users = await new UserDAO().getAllUsers();
+    setDatabaseEmpty(users.length === 0);
+  };
+
+  if (!loaded || isDatabaseEmpty) {
+    return (
+      <PaperProvider>
+        <ActivityIndicator animating={true} size="large" style={styles.centered} />
+      </PaperProvider>
+    ); // Pokaż spinner Material 3 kiedy aplikacja nie jest załadowana, umieszczony po środku ekranu
   }
 
-  return <RootLayoutNav />;
+  return (
+    <PaperProvider>
+      <RootLayoutNav />
+    </PaperProvider>
+  );
 }
 
 function RootLayoutNav() {
